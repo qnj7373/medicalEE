a5f467536a0bde6f7162d0b238884dc97148002b
qnj7373 <2898772052@qq.com>
2020-06-25 18:10:12 +0800

    shiro配置修改

--- src/main/java/org/wzxy/breeze/shiro/shiroConfig.java
+++ src/main/java/org/wzxy/breeze/shiro/shiroConfig.java
@@ -56,9 +56,6 @@ public class shiroConfig {
         Map<String,String> map = new LinkedHashMap<String, String>();
         //登录
         map.put("/system/login", "anon");
-        map.put("/user/toregister", "anon");
-        map.put("/personInfo/addPerson", "anon");
-        map.put("/pages/register.html", "anon");
         //对静态资源放行
         map.put("/images/**","anon");
         map.put("/assets/**","anon");
